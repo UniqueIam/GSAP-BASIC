87907b8759b251ea0ba05724736a35830dd42d1b
Abhimanyu Kumar <abhimanyukumar83097@gmail.com>
2024-08-02 20:50:33 +0530

    svg animation

--- script.js
+++ script.js
@@ -69,28 +69,80 @@
 //     duration:1.5
 // })
 
-var t1 = gsap.timeline();
+// var t1 = gsap.timeline();
 
-t1.from(".left",{
-    y:-30,
-    duration:1,
-    delay:0.5,
-    opacity:0
+// t1.from(".left",{
+//     y:-30,
+//     duration:1,
+//     delay:0.5,
+//     opacity:0
+// })
+// t1.from(".center p",{
+//     y:-30,
+//     duration:1,
+//     opacity:0,
+//     stagger:0.3
+// })
+// t1.from(".right",{
+//     y:-30,
+//     duration:1,
+//     opacity:0
+// })
+// t1.from("#detail",{
+//     y:20,
+//     duration:0.5,
+//     opacity:0,
+//     scale:0.2
+// })
+
+// Scroll Trigger Animation
+
+// gsap.from("#page1 #box",{
+//     rotate:360,
+//     duration:2,
+//     delay:1
+// })
+// gsap.from("#page2 p",{
+//     x:200,
+//     duration:2,
+//     opacity:0,
+//     scrollTrigger:{
+//         trigger:"#page2 p",
+//         scroller:"body",
+//         markers:"true",
+//         start:"top 60%",
+//         scrub:2
+//     }
+// })
+// gsap.from("#page3 #box",{
+//     rotate:360,
+//     duration:2,
+//     delay:1
+// })
+
+var path = `M 60 180 Q 400 180 790 180`;
+var finalPath = `M 60 180 Q 400 180 790 180`;
+
+var string = document.querySelector("#string");
+
+string.addEventListener("mousemove", function(dets) {
+    path = `M 60 180 Q ${dets.x} ${dets.y} 790 180`
+    console.log(path);
+    
+    gsap.to("svg path", {
+        attr: {d: path},
+        duration:0.2,
+        ease:"power3.out"
+    });
+});
+
+string.addEventListener("mouseleave",function(dets){
+    path = finalPath
+
+    gsap.to("svg path",{
+        attr:{d:path},
+        duration:1.8,
+        ease:"elastic.out"  
+    })
 })
-t1.from(".center p",{
-    y:-30,
-    duration:1,
-    opacity:0,
-    stagger:0.3
-})
-t1.from(".right",{
-    y:-30,
-    duration:1,
-    opacity:0
-})
-t1.from("#detail",{
-    y:20,
-    duration:0.5,
-    opacity:0,
-    scale:0.2
-})
+
